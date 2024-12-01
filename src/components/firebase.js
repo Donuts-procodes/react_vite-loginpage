3b51b65d4ffd993c8280d53074ded9ecbc503a50
Donuts-procodes <151659340+Donuts-procodes@users.noreply.github.com>
2024-12-01 23:14:53 +0530

    Update firebase.js

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import{getAuth} from "firebase/auth";
 import {getFirestore} from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyAjdgOgUwNv2LTx2y4Ay4MUoPdha0LIhM8",
-  authDomain: "vite-b533a.firebaseapp.com",
-  projectId: "vite-b533a",
-  storageBucket: "vite-b533a.firebasestorage.app",
-  messagingSenderId: "898113520768",
-  appId: "1:898113520768:web:88e5304ce8afc0ef3b9265"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 const app = initializeApp(firebaseConfig);
 export const auth=getAuth();
